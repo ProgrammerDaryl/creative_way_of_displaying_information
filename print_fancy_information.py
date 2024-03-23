bc612db5d76b33a85103a2ae1b1e79cdb9cfa5f8
ProgrammerDaryl <ProgrammerDaryl>
2024-03-23 19:28:01 +0800

    Request the User to Enter His or Her Name

---
+++ print_fancy_information.py
@@ -0,0 +1,13 @@
+# pseudocode
+
+# request the user to enter his or her name
+name = input("Enter your name here: ")
+# request the user to enter his or her dream job
+# request the user to enter his or her age
+# request the user to enter his or her place of birth
+# request the user to enter his or her religion
+# print his or her name in a creative way
+# print his or her dream job in a creative way
+# print his or her age in a creative way
+# print his or her place of birth in a creative way
+# print his or her religion in a creative way
